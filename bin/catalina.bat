a98633c743a0ab53be8d22a99458c41ad7e5998f
严格 <llabopmq@foxmail.com>
2024-10-05 19:10:58 +0800

    startup.bat和catalina.bat文件的作用

--- bin/catalina.bat
+++ bin/catalina.bat
@@ -109,6 +109,7 @@ rem ---------------------------------------------------------------------------
 setlocal
 
 rem Suppress Terminate batch job on CTRL+C
+rem 第一个参数不是run,就转向 mainEntry 标签,我们传递的第一个参数从上面来看是 start ,所以直接看 mainEntry 标签。
 if not ""%1"" == ""run"" goto mainEntry
 if "%TEMP%" == "" goto mainEntry
 if exist "%TEMP%\%~nx0.run" goto mainEntry
@@ -121,18 +122,25 @@ set RETVAL=%ERRORLEVEL%
 del /Q "%TEMP%\%~nx0.Y" >NUL 2>&1
 exit /B %RETVAL%
 :mainEntry
+rem 删除临时目录下的 catalina.bat.run 文件,如果出错也不提示
 del /Q "%TEMP%\%~nx0.run" >NUL 2>&1
 
 rem Guess CATALINA_HOME if not defined
+rem 设置 CURRENT_DIR 为当前目录bin(假如你是在bin目录中启动)
 set "CURRENT_DIR=%cd%"
+rem CATALINA_HOME 不是空就转向 gotHome
 if not "%CATALINA_HOME%" == "" goto gotHome
+rem 设置 CATALINA_HOME 指向 CURRENT_DIR 的目录,也就是 bin 目录
 set "CATALINA_HOME=%CURRENT_DIR%"
+rem 如果存在 %CATALINA_HOME%\bin\catalina.bat,那么就转向 okHome,否则退出到父目录(显然不存在bin\bin\catalina.bat)
 if exist "%CATALINA_HOME%\bin\catalina.bat" goto okHome
 cd ..
+rem 设置 CATALINA_HOME 为当前目录(也就是Tomcat的根目录）
 set "CATALINA_HOME=%cd%"
+rem 进入 CURRENT_HOME 指向的目录,也就是 bin 目录,执行 gotHome 标签
 cd "%CURRENT_DIR%"
 :gotHome
-
+rem 如果存在 %CATALINA_HOME%\bin\catalina.bat,就转向 okHome,现在 CATALINA_HOME 指向的目录是Tomcat的安装目录,所以 \bin\catalina.bat 文件是存在的,所以转向okHome标签
 if exist "%CATALINA_HOME%\bin\catalina.bat" goto okHome
 echo The CATALINA_HOME environment variable is not defined correctly
 echo This environment variable is needed to run this program
@@ -140,7 +148,9 @@ goto end
 :okHome
 
 rem Copy CATALINA_BASE from CATALINA_HOME if not defined
+rem 如果 CATALINA_BASE 变量不是空,就转向 gotBase 标签(显然为空)
 if not "%CATALINA_BASE%" == "" goto gotBase
+rem 设置 CATALINA_BASE 的值等同与 CATALINA_HOME 的值(现在CATALINA_BASE 也指向的Tomcat的根目录了)
 set "CATALINA_BASE=%CATALINA_HOME%"
 :gotBase
 
@@ -163,9 +173,11 @@ goto end
 
 rem Ensure that any user defined CLASSPATH variables are not used on startup,
 rem but allow them to be specified in setenv.bat, in rare case when it is needed.
+rem 清空CLASSPATH
 set CLASSPATH=
 
 rem Get standard environment variables
+rem 如果不存在 %CATALINA_BASE%\bin\setenv.bat 文件,那么就转向 checkSetenvHome 标签,我们现在使用的是apache-tomcat-8.5.42,该文件是不存在的,所以转向 checkSetenvHome 标签
 if not exist "%CATALINA_BASE%\bin\setenv.bat" goto checkSetenvHome
 call "%CATALINA_BASE%\bin\setenv.bat"
 goto setenvDone
@@ -174,30 +186,37 @@ if exist "%CATALINA_HOME%\bin\setenv.bat" call "%CATALINA_HOME%\bin\setenv.bat"
 :setenvDone
 
 rem Get standard Java environment variables
+rem 如果存在 %CATALINA_HOME%\bin\setclasspath.bat,那么就转向 okSetclasspath 标签(显然存在)
 if exist "%CATALINA_HOME%\bin\setclasspath.bat" goto okSetclasspath
 echo Cannot find "%CATALINA_HOME%\bin\setclasspath.bat"
 echo This file is needed to run this program
 goto end
 :okSetclasspath
+rem 调用 setclasspath.bat 并且传递第一个参数(start),有兴趣的同学可以自行查看setclasspath.bat文件做了什么,其实就是设置了JAVA_HOME,JRE_HOME等变量的值
 call "%CATALINA_HOME%\bin\setclasspath.bat" %1
 if errorlevel 1 goto end
 
 rem Add on extra jar file to CLASSPATH
 rem Note that there are no quotes as we do not want to introduce random
 rem quotes into the CLASSPATH
+rem 如果 CLASSPATH变量为空,就跳转到 emptyClasspath 标签,由上面能看出来 CLASSPATH 变量的确是空的,所以跳转到 emptyClasspath 标签
 if "%CLASSPATH%" == "" goto emptyClasspath
 set "CLASSPATH=%CLASSPATH%;"
 :emptyClasspath
+rem 设置 CLASSPATH 指向 %CLASSPATH%%CATALINA_HOME%\BIN\BOOTSTRAP.JAR(也就是TOMCAT安装目录下\BIN\BOOTSTRAP.JAR)
 set "CLASSPATH=%CLASSPATH%%CATALINA_HOME%\bin\bootstrap.jar"
-
+rem 如果 CATALINA_BASE 不为空(这里是为空)那么转向 gotBase 标签, gotBase 为空标签,继续往下执行,设置 CATALINA_BASE 等于 CATALINA_HOME
 if not "%CATALINA_TMPDIR%" == "" goto gotTmpdir
 set "CATALINA_TMPDIR=%CATALINA_BASE%\temp"
 :gotTmpdir
 
 rem Add tomcat-juli.jar to classpath
 rem tomcat-juli.jar can be over-ridden per instance
+rem 如果不存在 %CATALINA_BASE%\bin\tomcat-juli.jar 那么就跳转到 juliClasspathHome 标签,我们使用的tomcat版本是存在的，所以继续执行
 if not exist "%CATALINA_BASE%\bin\tomcat-juli.jar" goto juliClasspathHome
+rem 该命令set "CLASSPATH=%CLASSPATH%;%CATALINA_BASE%\bin\tomcat-juli.jar" 表示将tomcat-juli.jar的路径添加到 CLASSPATH 中
 set "CLASSPATH=%CLASSPATH%;%CATALINA_BASE%\bin\tomcat-juli.jar"
+rem 跳转 juliClasspathDone 标签,juliClasspathDone是空标签
 goto juliClasspathDone
 :juliClasspathHome
 set "CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\bin\tomcat-juli.jar"
@@ -211,13 +230,17 @@ set "JAVA_OPTS=%JAVA_OPTS% %JSSE_OPTS%"
 rem Register custom URL handlers
 rem Do this here so custom URL handles (specifically 'war:...') can be used in the security policy
 set "JAVA_OPTS=%JAVA_OPTS% -Djava.protocol.handler.pkgs=org.apache.catalina.webresources"
-
+rem 继续执行,如果 LOGGING_CONFIG 变量不为空就跳转到 noJuliConfig 标签,否则设置 LOGGING_CONFIG=-Dnop
 if not "%LOGGING_CONFIG%" == "" goto noJuliConfig
 set LOGGING_CONFIG=-Dnop
+rem 如果不存在 %CATALINA_BASE%\conf\logging.properties,那么就跳转 noJuliConfig 标签,否则设置 LOGGING_CONFIG等于-Djava.util.logging.config.file="%CATALINA_BASE%\conf\logging.properties"
+rem noJuliConfig 标签的作用就是设置 JAVA_OPTS 变量为 JAVA_OPTS+LOGGING_CONFIG
+rem 这里多说一句,我们可以在 catalina.bat 文件的开始位置添加 JAVA_OPTS 变量来指定jvm的配置,具体的参数可以自行搜索
 if not exist "%CATALINA_BASE%\conf\logging.properties" goto noJuliConfig
 set LOGGING_CONFIG=-Djava.util.logging.config.file="%CATALINA_BASE%\conf\logging.properties"
 :noJuliConfig
-
+rem 如果 LOGGING_MANAGER 为空就转向 noJuliManager 标签，否则设置 LOGGING_MANAGER 值为-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager
+rem noJuliManager 标签,将 LOGGING_MANAGER 添加到 JAVA_OPTS 变量
 if not "%LOGGING_MANAGER%" == "" goto noJuliManager
 set LOGGING_MANAGER=-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager
 :noJuliManager
@@ -245,6 +268,7 @@ rem ----- Execute The Requested Command ---------------------------------------
 echo Using CATALINA_BASE:   "%CATALINA_BASE%"
 echo Using CATALINA_HOME:   "%CATALINA_HOME%"
 echo Using CATALINA_TMPDIR: "%CATALINA_TMPDIR%"
+rem 打印相关信息,如果第一个参数等于 debug,转到 use_jdk 标签,否则打印相关信息,转向 java_dir_displayed 标签,java_dir_displayed 还是打印相关信息
 if ""%1"" == ""debug"" goto use_jdk
 echo Using JRE_HOME:        "%JRE_HOME%"
 goto java_dir_displayed
@@ -252,14 +276,15 @@ goto java_dir_displayed
 echo Using JAVA_HOME:       "%JAVA_HOME%"
 :java_dir_displayed
 echo Using CLASSPATH:       "%CLASSPATH%"
-
+rem 设置 _EXECJAVA 等于变量 _RUNJAVA.(该变量在 setclasspath.bat 中设置,值为 %JRE_HOME%\bin\java.exe
 set _EXECJAVA=%_RUNJAVA%
+rem 设置下相关变量,MAINCLASS,ACTION,清空 SECURITY_POLICY_FILE,DEBUG_OPTS,JPDA
 set MAINCLASS=org.apache.catalina.startup.Bootstrap
 set ACTION=start
 set SECURITY_POLICY_FILE=
 set DEBUG_OPTS=
 set JPDA=
-
+rem 如果第一个参数不等于jpda，转向 noJpda 标签.noJpda 为空标签,因为第一个参数是 start 所以,继续往下看
 if not ""%1"" == ""jpda"" goto noJpda
 set JPDA=jpda
 if not "%JPDA_TRANSPORT%" == "" goto gotJpdaTransport
@@ -279,6 +304,7 @@ shift
 
 if ""%1"" == ""debug"" goto doDebug
 if ""%1"" == ""run"" goto doRun
+rem 一开始就对第一个参数进行判断,进而转向不同的标签,因为我们第一个参数是start,所以我们直接就转向了doStart标签
 if ""%1"" == ""start"" goto doStart
 if ""%1"" == ""stop"" goto doStop
 if ""%1"" == ""configtest"" goto doConfigTest
@@ -315,11 +341,12 @@ shift
 echo Using Security Manager
 set "SECURITY_POLICY_FILE=%CATALINA_BASE%\conf\catalina.policy"
 goto execCmd
-
+rem doStart标签,shift,参数左移;如果 Title 变量为空就将 Title 设置为Tomcat,设置 _EXECJAVA 变量为 start "%TITLE%" %_RUNJAVA%
 :doStart
 shift
 if "%TITLE%" == "" set TITLE=Tomcat
 set _EXECJAVA=start "%TITLE%" %_RUNJAVA%
+rem 如果第一个参数不等于-security,转向 execCmd 标签
 if not ""%1"" == ""-security"" goto execCmd
 shift
 echo Using Security Manager
@@ -345,8 +372,22 @@ goto end
 
 :execCmd
 rem Get remaining unshifted command line arguments and save them in the
+rem 清空 CMD_LINE_ARGS 变量
 set CMD_LINE_ARGS=
 :setArgs
+rem 如果第一个变量为空,转向 doneSetArgs,运行到这里,因为前面在执行 doStart 的时候已经执行了shift命令,所以第一个参数已经是空了所,以直接转向 doneSetArgs 标签
+rem start "Tomcat" "D:\WorkSoftware\Java\jdk1.7.0_13\bin\java"
+rem -Djava.util.logging.config.file="E:\xxx\tomcat7.0\conf\logging.properties"
+rem -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager
+rem -Djava.endorsed.dirs="E:\xxx\tomcat7.0\endorsed"
+rem -classpath "E:\xxx\tomcat7.0\bin\bootstrap.jar;E:\xxx\tomcat7.0\bin\tomcat-juli.jar"
+rem -Dcatalina.base="E:\xxx\tomcat7.0"
+rem -Dcatalina.home="E:\xxx\tomcat7.0"
+rem -Djava.io.tmpdir="E:\xxx\tomcat7.0\temp" org.apache.catalina.startup.Bootstrap  start
+rem 新开一个窗口,窗口名字叫做Tomcat 使用java.exe,各种参数配置balabla,最后调用 org.apache.catalina.startup.Bootstrap类，(类入口当然是main方法)传递参数为start
+rem 总结:
+rem 1.tomcat启动入口是startup.bat文件,该文件会设置 CATALINA_HOME,CATALINA_BASE等常见变量,并且调用 catalina.bat文件,传递start参数;
+rem 2.catalina.bat文件,所做的事情就是检查关键文件是否存在,设置关键变量:例如CATALINA_HOME,CATALINA_BASE,classpath等等;最后新开一个窗口调用Bootstrap类的main方法,传递参数start,并且将一大堆配置传递过去
 if ""%1""=="""" goto doneSetArgs
 set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
 shift
--- bin/startup.bat
+++ bin/startup.bat
@@ -18,26 +18,52 @@ rem ---------------------------------------------------------------------------
 rem Start script for the CATALINA Server
 rem ---------------------------------------------------------------------------
 
+rem      该命令用于注释,rem起始的行不会作为代码执行
+rem pause    该命令用于暂停正在执行的批处理文件,并且提示用户按键，然后程序继续执行
+rem echo:     该命令用于在dos控制台显示一段文本,相当于print,如果想要显示环境变量需要在环境变量前后加上%,例如显示操作系统 echo %OS%
+rem echo off: 该命令可以防止将批处理文件中的具体命令打印出来,而只会输出执行结果。
+rem @echo off:该命令与echo off相同,唯一的区别在于 @echo off不仅会隐藏具体命令还会连'echo off'这个自身命令也隐藏起来
+rem set:      设置环境变量;例如 set A = 100 设置A变量为100
+rem label:    使用 ':'(冒号)来设置一个标签,供给goto命令使用;例如":init"代表一个init标签
+rem goto:     该命令使正在执行的命令强制跳转到他指定的标签;例如我需要跳转指定A标签下的命令:如下:goto A
+rem not:       该命令用来取反,相当于逻辑非
+rem if:       该命令表示判断
+rem exist:     该命令通常用来测试文件是否存在,一般和if一起使用
+rem shift:     该命令用来将参数后移一位即将%2%赋值给%1%,%3%赋值给%2%,也可以理解为参数列表左移即删除现有参数列表的第一位
+rem call:     该命令用来调用另外一条命令
+rem setLocal: 该命令表示该批处理文件中修改的环境变量只在本文件中起作用,或者直到endLocal命令出现;被修改的环境变量才恢复原状
+rem start:    重新开启一个dos窗口
+
 setlocal
 
 rem Guess CATALINA_HOME if not defined
+rem 设置变量 CURRENT_DIR 为当前目录;例如打开了tomcat的 bin 目录,那么 CURRENT_DIR 就指向了 bin 目录
 set "CURRENT_DIR=%cd%"
+rem 判断系统变量 CATALINA_HOME 如果不是空串,那么就跳到 gotHome 标签执行,实际很少设置 CATALINA_HOME 这个系统变量
 if not "%CATALINA_HOME%" == "" goto gotHome
+rem 设置 CATALINA_HOME 为 CURRENT_DIR 指向的目录,也就是 bin 目录
 set "CATALINA_HOME=%CURRENT_DIR%"
+rem 判断 CATALINA_HOME\bin\catalina.bat 文件是否存在,如果存在就转向 okHome 标签,明显 CATALINA_HOME 现在指向的是 bin 目录.那么 bin\bin\catalina.bat 文件是不存在的,所以继续往下看,不会跳转到 okHome
 if exist "%CATALINA_HOME%\bin\catalina.bat" goto okHome
+rem 退出到上一级目录
 cd ..
+rem 设置 CATALINA_HOME 指向当前目录,也就是Tomcat的根目录
 set "CATALINA_HOME=%cd%"
+rem 进入 CURRENT_DIR 指向的目录,也就是 bin 目录
 cd "%CURRENT_DIR%"
+rem 执行 gotHome 标签
 :gotHome
+rem 如果存在 CATALINA_HOME%\bin\catalina.bat 这个文件就跳转执行okHome标签
 if exist "%CATALINA_HOME%\bin\catalina.bat" goto okHome
 echo The CATALINA_HOME environment variable is not defined correctly
 echo This environment variable is needed to run this program
 goto end
 :okHome
-
+rem 设置变量 EXECUTABLE 指向 catalina.bat 文件
 set "EXECUTABLE=%CATALINA_HOME%\bin\catalina.bat"
 
 rem Check that target executable exists
+rem 双重保险继续判断下 EXECUTABLE 指向的 catalina.bat 文件是否存在.存在就跳转执行 okExec 标签
 if exist "%EXECUTABLE%" goto okExec
 echo Cannot find "%EXECUTABLE%"
 echo This file is needed to run this program
@@ -45,14 +71,17 @@ goto end
 :okExec
 
 rem Get remaining unshifted command line arguments and save them in the
+rem 表示清空变量CMD_LINE_ARGS
 set CMD_LINE_ARGS=
+rem 执行setArgs标签
 :setArgs
+rem 第一个变量(%1%)为空字符串,那么就跳转到 doneSetArgs 标签,因为我们是直接运行startup.bat,没有传递任何参数,所以我们应该是跳转到 doneSetArgs 标签,由此也可以猜想出,如果不是使用双击执行的话,使用命令行启动 startup.bat 那么是可以传递参数的
 if ""%1""=="""" goto doneSetArgs
 set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
 shift
 goto setArgs
 :doneSetArgs
-
+rem 跳转到 doneSetArgs,调用 EXECUTABLE 指向的文件,也就是 catalina.bat 文件,同时传递 start参数,因为 CMD_LINE_ARGS 为空,所以只传递了一个start参数
 call "%EXECUTABLE%" start %CMD_LINE_ARGS%
-
+rem rem 可以看出 end 标签是很多,判断失败跳转的标签,是参数不正确的时候的结束标志
 :end
