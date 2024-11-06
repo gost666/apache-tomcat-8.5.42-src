ddc19e16040c2ce8cc3a0fdde7c0782a1b3db66c
严格 <llabopmq@foxmail.com>
2024-11-06 09:10:55 +0800

    测试map接口相关实现

---
+++ test/test/testDataStructure.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.Test;
+
+import java.util.*;
+
+public class testDataStructure {
+    @Test
+    public void testMap() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("b", 12);
+
+    }
+}
